fd57bddb2462f8a2f42ba2fbdec26652ff8910d8
525750302 <46802084+525750302@users.noreply.github.com>
2023-11-17 16:35:39 +0900

    Create 2736.最大和查询.py

---
+++ 2736.最大和查询.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode.cn id=2736 lang=python
+#
+# [2736] 最大和查询
+#
+
+# @lc code=start
+class Solution(object):
+    def maximumSumQueries(self, nums1, nums2, queries):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+# @lc code=end
+
